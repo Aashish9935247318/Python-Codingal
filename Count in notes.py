d436ae9ba511f54b0c43bde83a42c04ac96a040f
Aashish9935247318 <Aashish32016@gmail.com>
2026-04-29 21:20:11 +0530

    notes

---
+++ Count in notes.py
@@ -0,0 +1,13 @@
+ # Talking total amount as input from user 
+Amount =int(input("Please Enter Amount for withdraw :"))
+
+ # Calucating the number of notes of different denomination
+note_1 = Amount//100
+note_2 = (Amount%100)//50
+note_3 = ((Amount%100)%50)//10
+
+
+
+print( "notes of 100 rupee" , note_1)
+print( "notes of 50 rupee" , note_2)
+print( "notes of 10 rupee" , note_3)
