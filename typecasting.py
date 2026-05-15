f111b2ffd30a061c688f576ad55a54f8b9fc9c93
Aashish9935247318 <Aashish32016@gmail.com>
2026-05-15 21:17:08 +0530

    type

---
+++ typecasting.py
@@ -0,0 +1,11 @@
+name = "Aashish"
+age = 10
+is_student = True
+weight = 28
+
+print("\n After Type Casting....")
+age = str(age)
+print("Data Type of age is", type(age))
+weight = int(weight)
+print(weight)
+print("Data Type of Weight is", type(weight))
