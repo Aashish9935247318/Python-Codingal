4d56f7912c3982e6162ab346f9dbfe5171bee0d3
Aashish9935247318 <Aashish32016@gmail.com>
2026-06-01 19:56:39 +0530

    ascii

---
+++ ASCII.py
@@ -0,0 +1,53 @@
+print(ord('A'))
+
+print(ord('a'))
+
+print(ord('0'))
+
+print(ord('@'))
+
+
+print(chr(65))
+
+print(chr(97))
+
+char = input("Enter a single character: ")
+
+
+
+if type(char) is str and len(char) == 1:
+
+    print("Valid input!")
+
+else:
+
+    print("Please enter extacly ONE character!")
+
+    ascii_val = ord(char)
+
+
+    print(f"Character: {char}")
+
+    print(f"ASCII Value: {ascii_val}")
+
+    # Identify character type using ASCII ranges
+
+if ascii_val >= 65 and ascii_val <= 90:
+
+    print("Type: Uppercase Letter")
+
+elif ascii_val >= 97 and ascii_val <= 122:
+
+    print("Type: Lowercase Letter")
+
+elif ascii_val >= 48 and ascii_val <= 57:
+
+    print("Type: Digit")
+
+elif ascii_val == 32:
+
+    print("Type: Space")
+
+else:
+
+    print("Type: Special Character")
