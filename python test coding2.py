87fdee31704a3ede28d171fc03f6673f530f832e
Aashish9935247318 <Aashish32016@gmail.com>
2026-07-17 21:17:13 +0530

    calculator2

---
+++ python test coding2.py
@@ -0,0 +1,17 @@
+num = int (input("Please Enter the First numeber: "))
+num =  int (input("Please Enter a Second Number: "))
+
+def add (a,b):
+   result (a+b)
+
+
+def subtract (b,c):
+   result (b-c)
+
+
+def multipiction (c,d):
+   result (b*c)
+
+
+def divide (d,e):
+   result (b/c)
