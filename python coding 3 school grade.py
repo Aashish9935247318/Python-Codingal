da05695834ecdbbe93682dbf4700179572510f33
Aashish9935247318 <Aashish32016@gmail.com>
2026-08-07 21:16:38 +0530

    grade

---
+++ python coding 3 school grade.py
@@ -0,0 +1,5 @@
+def grade_run_book():
+("name": "Harshit", "class III", "english", "science", "math"),
+("name": "David", "class III", "english", "science", "social science"),
+("name": "Hardik", "class III", "english", "science", "hindi"),
+("name": "Rohit", "class III", "g.k", "science", "social science"),
