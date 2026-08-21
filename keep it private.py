24894897d01fa2541bf53571c222a4f8a8e23d03
Aashish9935247318 <Aashish32016@gmail.com>
2026-08-21 20:57:29 +0530

    meth

---
+++ keep it private.py
@@ -0,0 +1,18 @@
+class MyClass:
+
+
+
+    __privateVar = 27;
+
+
+    def __privMeth(self):
+        print("I'm inside class MyClass")
+
+
+    def hello(self):
+        print("Private Variable value: ",myClass.__privateVar)
+
+
+foo = MyClass()
+foo.hello()
+foo.__privMeth
